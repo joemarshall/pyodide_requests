76b0e160dfc41e1cab6c1059a200e0eeaeb3129d
Mikko Ohtamaa <mikko@redinnovation.com>
2022-09-16 14:54:46 +0200

    A hack to see if we can get Pyodide build fixed

--- requests/__version__.py
+++ requests/__version__.py
@@ -5,7 +5,9 @@
 __title__ = 'requests'
 __description__ = 'Python HTTP for Humans.'
 __url__ = 'https://requests.readthedocs.io'
-__version__ = '2.26.0'
+# Spoofed to exact same version as in trading-strategy/pyproject.toml
+# to attempt to resolve Pyodide build issues
+__version__ = '2.28.1'  
 __build__ = 0x022600
 __author__ = 'Kenneth Reitz'
 __author_email__ = 'me@kennethreitz.org'
